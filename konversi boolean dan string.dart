b637f522738859cf9570f8445a63c0705670a41c
rafiqi <muhammadrafiqi171@gmail.com>
2024-10-07 11:29:37 +0700

    Create konversi boolean dan string.dart

---
+++ konversi boolean dan string.dart
@@ -0,0 +1,9 @@
+//INI ADALAH CONTOH KONVERSI TIPEDATA BOOLEAN KE STRING
+void main(){
+  var masukkanstring = 'true';
+  var masukanBoolean = masukkanstring == 'true';
+
+  var stringdariBoolean = masukanBoolean.toString();
+
+   print(stringdariBoolean);
+   }
